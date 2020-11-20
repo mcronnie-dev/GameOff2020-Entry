5c57acb658875d290664453fa08ab5c8d0973e5f
mcronnie-dev <mcronnie.dev@gmail.com>
2020-11-21 02:00:40 +0800

    clean hierarchy

---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    private Text _txtTimer;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _txtTimer = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _txtTimer.text = Time.time.ToString("F0"); //F0 sets the format of decimal no. places
+    }
+}
