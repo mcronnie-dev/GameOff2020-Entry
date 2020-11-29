b6113b55370870a8f343c8f77c7c004ecf6d73e3
Sarah Dominguez Perea <sdperea1@up.edu.ph>
2020-11-29 19:46:04 +0800

    Add Scenes BGM, buttons FX

---
+++ Assets/BtnFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnFX : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void PlayHoverFX() {
+        myFx.PlayOneShot(hoverFx);
+    }
+
+    public void PlayClickFX()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+
+}
