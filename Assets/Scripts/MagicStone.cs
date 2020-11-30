c616bec7aa4dab586dcbd4df3518aabd54590e3f
Sarah Dominguez Perea <sdperea1@up.edu.ph>
2020-11-30 15:13:12 +0800

    LoadNextLevel() working

--- Assets/Scripts/MagicStone.cs
+++ Assets/Scripts/MagicStone.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MagicStone : MonoBehaviour
 {
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
 
     public int StoneCount;
     public GameObject[] stoneAware;
@@ -21,6 +27,27 @@ public class MagicStone : MonoBehaviour
         StoneCount += 1;
 
         SetStoneCount(StoneCount);
+
+        if (StoneCount == 10) {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        // start animation
+        transition.SetTrigger("Start");
+
+        // wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // play scene
+        SceneManager.LoadScene(levelIndex);
     }
 
     public void Win()
